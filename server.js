5d09deb2fba9a8214cbf4167ed0904717e560f3a
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-01-18 12:45:09 +0530

    Search option created

--- server.js
+++ server.js
@@ -7,20 +7,25 @@ const express=require('express')
 const path=require('path')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
+
+
 const indexRouter=require('./routes/index')
-const mongoose=require('mongoose');
-
-
+const authorRouter=require('./routes/authors')
+const bodyParser=require('body-parser')
+const mongoose=require('mongoose')
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 app.listen(process.env.PORT || 3000)
 console.log('server running at port 3000')
 
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db=mongoose.connection
-db.on('error',error=>console.error(error));
+db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
